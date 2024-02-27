042ddfe4d1111444f24e62c057aa8400606974a1
Rufus <rufustrukhin@gmail.com>
2024-02-27 19:59:01 +0100

    Game Initial

---
+++ js/game.js
@@ -0,0 +1,7 @@
+/* Rufus Trukhin rutr7079 */
+document.addEventListener("DOMContentLoaded", function(){ //Needed for src to be in head    
+    $(document).ready(function() { //Needed for jquery
+
+
+    });
+}); 
