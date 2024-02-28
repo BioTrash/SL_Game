26025f8cbc8454f7e8864fa1afb2ecf386ce8591
Rufus <rufustrukhin@gmail.com>
2024-02-28 16:22:31 +0100

    Health Bar

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
 /* Rufus Trukhin rutr7079 */
 document.addEventListener("DOMContentLoaded", function(){ //Needed for src to be in head    
     $(document).ready(function() { //Needed for jquery
-
-
+          
     });
 }); 
