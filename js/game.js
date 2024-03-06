db53f1267e3b59022472e77198f2c3ec92a8402a
Rufus <rufustrukhin@gmail.com>
2024-03-06 22:06:03 +0100

    Base Conceptual UI Done

--- js/game.js
+++ js/game.js
@@ -1,6 +1,28 @@
 /* Rufus Trukhin rutr7079 */
 document.addEventListener("DOMContentLoaded", function(){ //Needed for src to be in head    
     $(document).ready(function() { //Needed for jquery
+        let enemyHealth = $('#currentHealth').css('width');
+        let parentWidth = $('#currentHealth').parent().width();
+
+        let percentage = (parseFloat(enemyHealth) / parentWidth) * 100.0;
+
+        function setHealth(modHealth){
+            $('#currentHealth').css('width', (percentage -= modHealth) + '%');
+        }
+        
+        $("#enemy").click(function(){
+            setHealth(.1);
+          });
           
+
+        $("#topLeft").click(function(){
+            setInterval(function(){
+                setHealth(.1);
+            }, 100);     
+          });
+
+        $("#confirm").click(function(){
+            $("#invisible").hide();
+        });
     });
 }); 
